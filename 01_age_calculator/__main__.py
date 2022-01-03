e1ae5b8502b201e37cddcb6267cd6ba4bc892fcf
Esteban Borai <estebanborai@gmail.com>
2022-01-02 21:56:06 -0300

    feat: add missing documentation

--- 01_age_calculator/__main__.py
+++ 01_age_calculator/__main__.py
@@ -1,5 +1,11 @@
+# Asks user for it's name and stores it into the `name` variable
 name = input('Enter your name: ')
+# Asks user for it's age, then parses text into a number and
+# finally stores into the `age` variable
 age = int(input('Enter your age: '))
-current_year = 2021
+# Stores the current year into `current_year` variable.
+# Note: This is HARDCODED if year changes this won't be updated
+current_year = 2022
 
+# Prints the sentence to the output
 print(name + ' born on ' + str(current_year - age))
